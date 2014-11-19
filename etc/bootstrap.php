b4555869e77884187acdcccf244ef625410d23e6
Mark Kimsal <metrofindings@gmail.com>
2014-11-19 09:54:05 -0500

    Sample files.

---
+++ etc/bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+_iCanHandle('analyze',   'metrofw/analyzer.php');
+_iCanHandle('analyze',   'metrofw/router.php', 3);
+_iCanHandle('resources', 'metrofw/utils.php');
+_iCanHandle('output',    'metrofw/output.php');
+//will be removed if output.php doesn't think we need HTML output
+_iCanHandle('output',    'metrofw/template.php', 3);
+_iCanHandle('exception', 'metrofw/exdump.php::onException');
+_iCanHandle('hangup',    'metrofw/output.php');
+
+_didef('request',        'metrofw/request.php');
+_didef('response',       'metrofw/response.php');
+_didef('router',         'metrofw/router.php');
+
+_set('template_basedir', 'templates/');
+_set('template_baseuri', 'templates/');
+_set('template_name',    'webapp01');
+
+_set('route_rules',  array() );
+
+_set('route_rules',
+	array_merge(array('/:appName'=>array( 'modName'=>'main', 'actName'=>'main' )),
+	_get('route_rules')));
+
+_set('route_rules',
+	array_merge(array('/:appName/:modName'=>array( 'actName'=>'main' )),
+	_get('route_rules')));
+
+_set('route_rules',
+	array_merge(array('/:appName/:modName/:actName'=>array(  )),
+	_get('route_rules')));
+
+_set('route_rules',
+	array_merge(array('/:appName/:modName/:actName/:arg'=>array(  )),
+	_get('route_rules')));
