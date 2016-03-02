1b4fe197ffd06f3f88379ce29801b2346958566c
Mark Kimsal <metrofindings@gmail.com>
2016-03-02 18:36:13 -0500

    Allow dynamic printing of response items.
    When no template is present we should print items in the response
    items that match the current section.

--- src/template/rain.php
+++ src/template/rain.php
@@ -40,25 +40,66 @@ class Template_Rain {
 			}
 		);
 
+		$viewFile = _get('template.main.file', $request->modName.'_'.$request->actName);
 		try {
 			$t = new Tpl;
 			$t->var = $response->sectionList;
 			$t->assign('baseurl', m_url());
 			$t->assign('turl', m_turl());
 			ob_start();
-			$t->draw($request->modName.'_'.$request->actName);
+			$t->draw($viewFile);
 			$x =  ob_get_contents();
 			ob_end_clean();
 			echo $x;
-		} catch (Exception $e) {
-			if ($request->isDevelopment()) {
-				throw $e;
+		} catch (\Rain\Tpl\NotFoundException $e) {
+			if ($response->has('main')) {
+				if (is_array($response->main)) {
+					$mainArray = $response->main;
+				} else {
+					$mainArray = array($response->main);
+				}
+
+				foreach ($mainArray as $_itm) {
+					echo "<section>";
+					echo $this->transformContent($_itm);
+					echo "</section>";
+				}
 			}
+		} catch (Exception $e) {
 			//output.php::hangup() will set the header for 500
 			//if we are in production mode, we can set apache or
 			// nginx in cgi mode to intercept status headers and
 			// show a custom 500/501 page.
 			$response->statusCode = 500;
+			if ($request->isDevelopment()) {
+				throw $e;
+			}
+		}
+	}
+
+	public function transformContent($content) {
+
+		//struct
+		if (is_array($content)) {
+			return implode(' ', array_values($content));
+		}
+
+		//we have some special output,
+		// could be text, could be object
+		if (!is_object($content))
+			return $content;
+
+		//it's an object
+		if (method_exists( $content, 'toHtml' )) {
+			return call_user_func(array($content, 'toHtml'));
+		}
+
+		if (method_exists( $content, 'toString' )) {
+			return call_user_func(array($content, 'toString'));
+		}
+
+		if (method_exists( $content, '__toString' )) {
+			return call_user_func(array($content, '__toString'));
 		}
 	}
 }
